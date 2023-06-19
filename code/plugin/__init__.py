2fc89c4144c4a47cb8fa7aeff958e9ee674b485e
windwhiterain <1712428442@qq.com>
2023-06-19 10:15:20 +0800

    Create __init__.py

---
+++ code/plugin/__init__.py
@@ -0,0 +1,15 @@
+def reload(path:str):
+    """
+    load a plugin with its relative path in Elf/plugin,override the loaded one with same path if exist
+    :param path: the plugin's relative path in Elf/plugin
+    :return:
+    """
+    #TODO
+    pass
+def load_all():
+    """
+    load all plugins in Elf/plugin
+    :return:
+    """
+    #TODO
+    pass
