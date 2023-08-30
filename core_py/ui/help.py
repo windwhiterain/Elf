717e88c4c372656189ef9c5211fda5e66a59c400
windwhiterain <1712428442@qq.com>
2023-08-30 23:58:16 +0800

    ui node

--- core_py/ui/help.py
+++ core_py/ui/help.py
@@ -3,6 +3,7 @@ from PySide6.QtWidgets import QWidget,QLabel,QTableWidget
 from ui.palette import ColorLink
 from PySide6.QtGui import QColor,QPalette
 from PySide6.QtGui import QAction
+from typing import TypeVar
 def set_geo(widget:QWidget,center:tuple[int,int],size:tuple[int,int]):
     widget.setGeometry(center[0]-size[0]/2,center[1]-size[1]/2,size[0],size[1])
 def set_geof(widget:QWidget,center:tuple[float,float],size:tuple[float,float]):
@@ -17,4 +18,28 @@ def newQAction(text:str,parent,callback)->QAction:
     ret=QAction(text=text,parent=parent)
     ret.triggered.connect(callback)
     return ret
+def tuplize(vec)->tuple[float,float]:
+    return (vec.x(),vec.y())
+def add(t0:tuple[float,float],t1:tuple[float,float])->tuple[float,float]:
+    return (t0[0]+t1[0],t0[1]+t1[1])
+def sub(t0:tuple[float,float],t1:tuple[float,float])->tuple[float,float]:
+    return (t0[0]-t1[0],t0[1]-t1[1])
+def center(widget:QWidget)->tuple[float,float]:
+    geo=widget.geometry()
+    return (geo.x()+geo.width()/2,geo.y()+geo.height()/2)
+def size(widget:QWidget)->tuple[float,float]:
+    return (widget.width(),widget.height())
+def set_center(widget:QWidget,center:tuple[float,float]):
+    set_geof(widget,center,size(widget))
+def set_size(widget:QWidget,size:tuple[float,float]):
+    set_geof(widget,center(widget),size)
+T = TypeVar("T")
+def resize(arr:list[T],size:int,gen,des)->list[T]:
+    arr_len=len(arr)
+    if size>arr_len:
+        arr.append(gen())
+        resize(arr,size,gen,des)
+    elif size<len(arr):
+        des(arr.pop(arr_len-1))
+        resize(arr,size,gen,des)
 
--- core_py/ui/widgets/network_panel.py
+++ core_py/ui/widgets/network_panel.py
@@ -13,25 +13,99 @@ from enum import Enum
 import elf_rust
 from elf_rust import NodeType
 
-class Node(QLabel):
+class NodePort(QLabel):
+    def __init__(self,node:'Node'):
+        super().__init__()
+        self.node=node
+        self.setStyleSheet(node.qss)
+        self.setParent(node)
+        self.show()
+class NodeMain(QLabel):
+    def __init__(self,node:'Node'):
+        super().__init__()
+        self.node=node
+        self.setParent(node)
+    def mousePressEvent(self, e):
+        self.node.start_mouse_pos=tuplize(e.globalPos())
+        match e.button():
+            case Qt.MouseButton.LeftButton:
+                self.node.on_drag=True
+                self.node.start_center=center(self.node)
+            case Qt.MouseButton.RightButton:
+                self.node.on_port=True
+                self.node.start_input_port_count=len(self.node.input_ports)
+                self.node.start_output_port_count=len(self.node.output_ports)
+    def mouseReleaseEvent(self, e):
+        match e.button():
+            case Qt.MouseButton.LeftButton:
+                self.node.on_drag=False
+            case Qt.MouseButton.RightButton:
+                self.node.on_port=False
+    def mouseMoveEvent(self, e):
+        if self.node.on_drag:
+            self.node.move_with_mouse(e)
+        if self.node.on_port:
+            self.node.scale_port_count(e)
+        
+class Node(QWidget):
     def __init__(self,pos:tuple[float,float],node_type:NodeType,index:int):
         super().__init__()
-        set_geof(self,pos,(90,30))
+        set_center(self,pos)
+        self.input_ports=list[NodePort]()
+        self.output_ports=list[NodePort]()
+        self.on_drag=False
+        self.on_port=False
         self.index=index
+        self.node_main=NodeMain(self)
         self.color_sheet=node
         match node_type:
             case NodeType.DataOperator:
                 frame_color=self.color_sheet.frame_data_operator
             case NodeType.InterfaceOperator:
                 frame_color=self.color_sheet.frame_interface_operator
-        self.setStyleSheet("""
+        self.qss="""
             QLabel{
                 background-color:"""+self.color_sheet.bg.get().name()+""";
                 border-style:solid;
                 border-width:2px;
                 border-color:"""+frame_color.get().name()+""";
             }
-        """)
+        """
+        self.setStyleSheet(self.qss)
+        self.refresh(1,1)
+        self.refresh(5,5)
+        self.refresh(1,1)
+    def move_with_mouse(self,e):
+        d_mouse_pos=sub(tuplize(e.globalPos()),self.start_mouse_pos)
+        goal_pos=add(self.start_center,d_mouse_pos)
+        set_center(self,goal_pos)
+    def scale_port_count(self,e):
+        d_x=e.globalPos().x()-self.start_mouse_pos[0]
+        d_count_in=round(d_x/20)
+        goal_count_in=max(self.start_input_port_count+d_count_in,1)
+        d_y=e.globalPos().y()-self.start_mouse_pos[1]
+        d_count_out=round(d_y/20)
+        goal_count_out=max(self.start_output_port_count+d_count_out,1)
+        self.refresh(goal_count_in,goal_count_out)
+
+    def refresh(self,input_port_count:int,output_port_count:int):
+        count=max(input_port_count,output_port_count)
+        width=60+count*30
+        set_size(self,(width,50))
+        set_geof(self.node_main,(self.width()/2,self.height()/2),(self.width(),30))
+        gen_port=lambda :NodePort(self)
+        des_port=lambda port:(port.deleteLater())
+        assert(self.input_ports is not None)
+        resize(self.input_ports,input_port_count,gen_port,des_port)
+        resize(self.output_ports,output_port_count,gen_port,des_port)
+        assert(self.input_ports is not None)
+        
+        print(input_port_count)
+        for index,port in enumerate(self.input_ports):
+            set_geof(port,((index+1)/(input_port_count+1)*self.width(),5),(8,8))
+        for index,port in enumerate(self.output_ports):
+            set_geof(port,((index+1)/(output_port_count+1)*self.width(),self.height()-5),(8,8))
+        
 
 class NetworkPanel(QWidget):
     def __init__(self):
